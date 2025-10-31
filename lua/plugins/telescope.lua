713658ea6cdecd73bb45b6fd83b9a6ca35c05cc3
jpenagos32 <jpenagosdev@gmail.com>
2025-10-30 23:58:27 -0500

    Agregar treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,6 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
     vim.keymap.set('n', '<leader>ps', function()
