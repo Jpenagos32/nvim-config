cb86f22cbe2e43926a4dbaa7a645124143be240f
jpenagos32 <jpenagosdev@gmail.com>
2025-11-01 04:03:33 -0500

    Agregar comentarios a los remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", ":/")
-vim.keymap.set("n", "<leader>t", ":botright 8sp|term<CR>")
+vim.keymap.set("n", "<leader>t", ":botright 8sp|term<CR>") --Abre una terminal en la parte inferior
 vim.keymap.set("n", "<leader>+", "2<C-w>+") -- aumenta el tamaño de la ventana en split
 vim.keymap.set("n", "<leader>-", "2<C-w>-")-- disminute el tamaño de la ventana split
 vim.keymap.set("n", "<leader>w", "<C-w>w")-- cambia entre ventanas
