59ea49de2eeb7765d45603b35a5cbcb1d7145210
jpenagos32 <jpenagosdev@gmail.com>
2025-11-01 03:59:49 -0500

    Agregar un comando para abrir una terminal de tamaño predefinido

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", ":/")
-vim.keymap.set("n", "<leader>t", vim.cmd.terminal)
-vim.keymap.set("n", "<leader>+", "<C-w>+") -- aumenta el tamaño de la ventana en split
-vim.keymap.set("n", "<leader>-", "<C-w>-")-- disminute el tamaño de la ventana split
+vim.keymap.set("n", "<leader>t", ":botright 8sp|term<CR>")
+vim.keymap.set("n", "<leader>+", "2<C-w>+") -- aumenta el tamaño de la ventana en split
+vim.keymap.set("n", "<leader>-", "2<C-w>-")-- disminute el tamaño de la ventana split
 vim.keymap.set("n", "<leader>w", "<C-w>w")-- cambia entre ventanas
