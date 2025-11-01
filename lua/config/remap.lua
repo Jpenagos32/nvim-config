47b679b6da1b8a04ea8ff68bfd29846972a72dd4
jpenagos32 <jpenagosdev@gmail.com>
2025-11-01 03:46:00 -0500

    Agregar plugin para git

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,6 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", ":/")
 vim.keymap.set("n", "<leader>t", vim.cmd.terminal)
-vim.keymap.set("n", "<leader>+", "<C-w>+")-- aumenta el tamaño de la ventana en split
+vim.keymap.set("n", "<leader>+", "<C-w>+") -- aumenta el tamaño de la ventana en split
 vim.keymap.set("n", "<leader>-", "<C-w>-")-- disminute el tamaño de la ventana split
 vim.keymap.set("n", "<leader>w", "<C-w>w")-- cambia entre ventanas
