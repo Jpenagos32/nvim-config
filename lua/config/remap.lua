f178c7ba9720733cf30bb5f1fd2ad45b1011b3d9
jpenagos32 <jpenagosdev@gmail.com>
2025-11-01 04:40:28 -0500

    Agregar comentarios a los atajos creados

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>f", ":/")
-vim.keymap.set("n", "<leader>t", ":botright 8sp|term<CR>") --Abre una terminal en la parte inferior
-vim.keymap.set("n", "<leader>+", "2<C-w>+") -- aumenta el tamaño de la ventana en split
-vim.keymap.set("n", "<leader>-", "2<C-w>-")-- disminute el tamaño de la ventana split
-vim.keymap.set("n", "<leader>w", "<C-w>w")-- cambia entre ventanas
+vim.keymap.set("n","<leader>pv", vim.cmd.Ex, {desc = 'Abre el explorador'})
+vim.keymap.set("n", "<leader>f", ":/", {desc = 'Busca palabras dentro del archivo'})
+vim.keymap.set("n", "<leader>t", ":botright 8sp|term<CR>", {desc = 'Abre una terminal en la parte inferior'}) 
+vim.keymap.set("n", "<leader>+", "2<C-w>+", {desc = 'Aumenta el tamaño de la ventana en split'}) 
+vim.keymap.set("n", "<leader>-", "2<C-w>-", {desc = 'Disminuye el tamaño de la ventana split'})
+vim.keymap.set("n", "<leader>w", "<C-w>w", {desc = 'Cambia entre ventanas split'})
