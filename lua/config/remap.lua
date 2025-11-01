a59b0af2a3790784805e721da593f0f7f5c978d6
jpenagos32 <jpenagosdev@gmail.com>
2025-11-01 03:33:31 -0500

    Agregar atajos para cambio de ventanas y redimensionamiento de ventanas

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", ":/")
+vim.keymap.set("n", "<leader>t", vim.cmd.terminal)
+vim.keymap.set("n", "<leader>+", "<C-w>+")-- aumenta el tamaño de la ventana en split
+vim.keymap.set("n", "<leader>-", "<C-w>-")-- disminute el tamaño de la ventana split
+vim.keymap.set("n", "<leader>w", "<C-w>w")-- cambia entre ventanas
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,15 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  -- or                              , branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end, { desc = 'Telescope grep string' })
-  end,
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end, { desc = 'Telescope grep string' })
+    end,
 }
