5670e2b8959c0acccecc873c1aface27bd2dff03
jpenagos32 <jpenagosdev@gmail.com>
2025-11-05 23:54:34 -0500

    Eliminar los atajos para copiar desde y hacia la papelera

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex, {desc = 'Abre el explorador'})
-vim.keymap.set("n", "<leader>f", ":/", {desc = 'Busca palabras dentro del archivo'})
+vim.keymap.set("n", "<leader>ft", ":/", {desc = 'Busca palabras dentro del archivo'})
 vim.keymap.set("n", "<leader>t", ":botright 8sp|term<CR>", {desc = 'Abre una terminal en la parte inferior'}) 
 vim.keymap.set("n", "<leader>+", "2<C-w>+", {desc = 'Aumenta el tamaño de la ventana en split'}) 
 vim.keymap.set("n", "<leader>-", "2<C-w>-", {desc = 'Disminuye el tamaño de la ventana split'})
