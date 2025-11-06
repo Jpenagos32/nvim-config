596f2a08527347321370a9645bacd3e15e24b169
jpenagos32 <jpenagosdev@gmail.com>
2025-11-05 23:53:16 -0500

    Agregar configuracion para copiar y pegar a la papelera

--- lua/config/basics.lua
+++ lua/config/basics.lua
@@ -3,6 +3,9 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.wrap = false -- es para el linewrap
+vim.opt.clipboard = "unnamedplus" -- sincroniza la papelera de neovim con la papelera del sistema operativo
 
 -- Habilita texto informativo
 vim.diagnostic.config({
