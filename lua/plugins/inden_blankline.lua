b16c2621754caf2f78cb25b6d1a5ff84ca87c674
jpenagos32 <jpenagosdev@gmail.com>
2025-11-07 19:44:52 -0500

    Excluir los archivos de texto de la configuracion de indentado

--- lua/plugins/inden_blankline.lua
+++ lua/plugins/inden_blankline.lua
@@ -26,6 +26,16 @@ return {
             vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
         end)
 
-        require("ibl").setup { indent = { highlight = highlight } }
+        require("ibl").setup {
+            indent = {
+                highlight = highlight
+            },
+            exclude = {
+                filetypes = {
+                    "text",
+                    "terminal",
+                }
+            }
+        }
     end
 }
