7c78885f614cbb8b88af784fa9d14afc2e55f583
jpenagos32 <jpenagosdev@gmail.com>
2025-11-10 21:22:43 -0500

    Desactivar configuracion TextChanged de auto-save para compatibilidad con Harpoon2

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -6,8 +6,17 @@ return {
     opts = {
         enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
         trigger_events = { -- See :h events
-            immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
-            defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+            immediate_save = {
+                "BufLeave",
+                "FocusLost",
+                "QuitPre",
+                "VimSuspend"
+            }, -- vim events that trigger an immediate save
+
+            defer_save = {
+                "InsertLeave",
+                -- "TextChanged" -- Desactivado para compatibilidad con harpoon
+            }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
             cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
         },
         -- function that takes the buffer handle and determines whether to save the current buffer or not
