2c1f34bc1753cd1e104381d2cd3f4f99de5bbc55
jpenagos32 <jpenagosdev@gmail.com>
2025-11-10 20:30:35 -0500

    Arreglar problema con treesitter objects

--- lua/plugins/Treesitter.lua
+++ lua/plugins/Treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-        'nvim-treesitter-textobjects'
+        'nvim-treesitter/nvim-treesitter-textobjects'
     },
     build = ":TSUpdate",
     event = 'VeryLazy',
