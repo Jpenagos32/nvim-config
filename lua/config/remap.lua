3e2323812369d7cdf3bc21e8235c5bb09e503e2f
Julian Penagos <jpenagosdev@gmail.com>
2026-01-05 10:39:15 -0500

    Agregar configuracion para autocompletado de comillas, llaves y corchetes

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,24 +1,25 @@
 vim.g.mapleader = " "
+local keymap = vim.keymap.set
 
 -- Explorador
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Abre el explorador' })
-vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+-- keymap("n", "<leader>pv", vim.cmd.Ex, { desc = 'Abre el explorador' })
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
 -- Archivos
-vim.keymap.set("n", "<leader>ft", "/", { desc = 'Busca palabras dentro del archivo' })
-vim.keymap.set("n", "<A-S-k>", ":m -2<CR>", { desc = 'Sube la linea actual' })
-vim.keymap.set("n", "<A-S-j>", ":m +1<CR>", { desc = 'Baja la linea actual' })
-vim.keymap.set("n", "<leader>i", "gg=G", { desc = 'Indentacion de todo el archivo' })
+keymap("n", "<leader>ft", "/", { desc = 'Busca palabras dentro del archivo' })
+keymap("n", "<A-S-k>", ":m -2<CR>", { desc = 'Sube la linea actual' })
+keymap("n", "<A-S-j>", ":m +1<CR>", { desc = 'Baja la linea actual' })
+keymap("n", "<leader>i", "gg=G", { desc = 'Indentacion de todo el archivo' })
 
 -- Reemplazar
-vim.keymap.set("n", "<leader>rg", ":%s/",
+keymap("n", "<leader>rg", ":%s/",
     {
         desc =
         'Reemplaza todas las ocurrencias de la palabra dentro del archivo (usar el flag /c al final para preguntar)'
     }
 )
 
-vim.keymap.set(
+keymap(
     "n",
     "<leader>rw",
     ":s/",
@@ -26,81 +27,58 @@ vim.keymap.set(
 )
 
 -- Ventanas
-vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = 'Divide la ventana horizontalmente' })
-vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = 'Divide la ventana verticalmente' })
-vim.keymap.set("n", "<leader>ww", "<C-w>w", { desc = 'Cambia entre ventanas split' })
-vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = 'Cambia a la ventana inferior.' })
-vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = 'Cambia a la ventana superior' })
-vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = 'Cambia a la ventana derecha' })
-vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = 'Cambia a la ventana izquierda' })
-vim.keymap.set("n", "<leader>w_", "<C-w>_", { desc = 'Maximiza el alto de la ventana' })
-vim.keymap.set("n", "<leader>w|", "<C-w>|", { desc = 'Maximiza el ancho de la ventana' })
-vim.keymap.set("n", "<leader>w=", "<C-w>=", { desc = 'Iguala el tamaño de todas las ventanas' })
-vim.keymap.set("n", "<leader>t", ":term<CR>i", { desc = 'Abre una terminal', silent = true })
-vim.keymap.set("n", "<leader>+", "2<C-w>+", { desc = 'Aumenta el tamaño de la ventana en split' })
-vim.keymap.set("n", "<leader>-", "2<C-w>-", { desc = 'Disminuye el tamaño de la ventana split' })
+keymap("n", "<leader>ws", "<C-w>s", { desc = 'Divide la ventana horizontalmente' })
+keymap("n", "<leader>wv", "<C-w>v", { desc = 'Divide la ventana verticalmente' })
+keymap("n", "<leader>ww", "<C-w>w", { desc = 'Cambia entre ventanas split' })
+keymap("n", "<leader>wj", "<C-w>j", { desc = 'Cambia a la ventana inferior.' })
+keymap("n", "<leader>wk", "<C-w>k", { desc = 'Cambia a la ventana superior' })
+keymap("n", "<leader>wl", "<C-w>l", { desc = 'Cambia a la ventana derecha' })
+keymap("n", "<leader>wh", "<C-w>h", { desc = 'Cambia a la ventana izquierda' })
+keymap("n", "<leader>w_", "<C-w>_", { desc = 'Maximiza el alto de la ventana' })
+keymap("n", "<leader>w|", "<C-w>|", { desc = 'Maximiza el ancho de la ventana' })
+keymap("n", "<leader>w=", "<C-w>=", { desc = 'Iguala el tamaño de todas las ventanas' })
+keymap("n", "<leader>t", ":term<CR>i", { desc = 'Abre una terminal', silent = true })
+keymap("n", "<leader>+", "2<C-w>+", { desc = 'Aumenta el tamaño de la ventana en split' })
+keymap("n", "<leader>-", "2<C-w>-", { desc = 'Disminuye el tamaño de la ventana split' })
 
 -- buffers
-vim.keymap.set("n", "gb", ":bn<CR>", {
+keymap("n", "gb", ":bn<CR>", {
     desc = 'Cambia al buffer siguiente',
     noremap = true,
     silent = true
 })
 
-vim.keymap.set("n", "gB", ":bp<CR>", {
+keymap("n", "gB", ":bp<CR>", {
     desc = 'Cambia al buffer anterior',
     noremap = true,
     silent = true
 })
 
-vim.keymap.set("n", "<leader>bd", ":bd<CR>", {
+keymap("n", "<leader>bd", ":bd<CR>", {
     desc = 'Elimina el buffer actual',
     noremap = true,
     silent = true
 })
 
-vim.keymap.set("n", "<leader>ba", ":%bd<CR>", {
+keymap("n", "<leader>ba", ":%bd<CR>", {
     desc = 'Elimina TODOS los buffers',
     noremap = true,
     silent = true
 })
 
 -- Terminal
-vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>", { desc = 'Sale del modo terminal y vuelve al modo normal' })
-
--- Code actions
-vim.keymap.set("n", "<leader>aa", "gra", {
-    desc = 'Muestra los code actions',
-    noremap = true
-})
-
-vim.keymap.set("n", "<leader>ai", "gri", {
-    desc = 'Muestra las implementaciones del símbolo bajo el cursor',
-    noremap = true
-})
-
-vim.keymap.set("n", "<leader>an", "grn", {
-    desc = 'Renombra todas las referencias del simbolo bajo el cursor',
-    noremap = true
-})
-
-vim.keymap.set("n", "<leader>ar", "grr", {
-    desc = 'Muestra todas las referencias del símbolo bajo el cursor',
-    noremap = true
-})
-
-vim.keymap.set("n", "<leader>at", "grt", {
-    desc = 'Salta a la definicion del tipo del símbolo bajo el cursor',
-    noremap = true
-})
-
-vim.keymap.set("n", "<leader>at", "grt", {
-    desc = 'Salta a la definicion del tipo del símbolo bajo el cursor',
-    noremap = true
-})
+keymap("t", "<C-\\>", "<C-\\><C-n>", { desc = 'Sale del modo terminal y vuelve al modo normal' })
 
 -- Markdown
-vim.keymap.set('n', '<leader>mp', ':MarkdownPreviewToggle<CR>', {
+keymap('n', '<leader>mp', ':MarkdownPreviewToggle<CR>', {
     desc = 'Inicia o detiene el m Markdown Preview',
     silent = true
 })
+
+-- Autocompletado
+keymap('i', '"', '""<left>', { noremap = true, silent = true })
+keymap("i", "'", "''<left>", { noremap = true, silent = true })
+keymap('i', '(', '()<left>', { noremap = true, silent = true })
+keymap('i', '[', '[]<left>', { noremap = true, silent = true })
+keymap('i', '{', '{}<left>', { noremap = true, silent = true })
+keymap('i', '<', '<><left>', { noremap = true, silent = true })
